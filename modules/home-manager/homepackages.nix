4c3738a3d26f356d934969fb021fc36bbc1d3a8f
Jack Mechem <mechemjack@gmail.com>
2026-01-27 16:23:05 -0800

    Fix homepackages

---
+++ modules/home-manager/homepackages.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    pkgs.sway-contrib.grimshot
+    waypaper
+    hyprpaper
+    swaybg
+    lua-language-server
+    nil
+    nixfmt-rfc-style
+    stylua
+
+    lunar-client
+    discord
+
+    spotify
+    pavucontrol
+
+    zoom-us
+  ];
+}
